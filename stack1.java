9556188ed9333fa3dc17fafad79f66ddbe5d7b48
Rudhra Sree <rudhrasree2005_bee27@mepcoeng.ac.in>
2026-06-18 06:11:28 +0530

    Create stack1.java

---
+++ stack1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Solution 
+{
+    public boolean isValid(String s) 
+    {
+            Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            else {
+
+                if(stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+
+                if((ch == ')' && top != '(') ||
+                   (ch == '}' && top != '{') ||
+                   (ch == ']' && top != '['))
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+
+        String a = scan.nextLine();
+
+        Solution sol = new Solution();
+
+        boolean b = sol.isValid(a);
+
+        System.out.print(b);
+    }
+}
